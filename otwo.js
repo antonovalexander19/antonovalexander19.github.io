a91d27e2975f4247141162c0e30f06f084d1e9cd
antonovalexander19 <a.antonov1347@gmail.com>
2024-12-11 07:17:21 +0300

    Update otwo.js

--- otwo.js
+++ otwo.js
@@ -1,17 +1,26 @@
 function find_editf(){
-    const item = document.getElementById('new')
+    const item = document.getElementById('newa')
     console.log(item.innerText)
-    item.innerHTML=<b>Antonov</b>
+    item.innerHTML="Antonov"
 }
 function find_edits(){
-    const item = document.getElementById('new2')
+    const item = document.getElementById('newb')
     console.log(item.innerText)
-    item.innerHTML=<b>Antonov</b>
+    item.innerHTML="Alexander"
 }
 function find_editt(){
-    const item = document.getElementById('new3')
+    const item = document.getElementById('newc')
     console.log(item.innerText)
-    item.innerHTML=<b>Antonov</b>
+    item.innerHTML="Evgenyevich"
+    
+}
+function find_editfo(){
+    const item = document.getElementById('newd')
+    console.log(item.innerText)
+    item.innerHTML="town Yoshkar-Ola, Mary El republic, Russian Federation<"
 }
 const node_for_click = document.getElementById("for_click")
 node_for_click.addEventListener("click",find_editf)
+node_for_click.addEventListener("click",find_edits)
+node_for_click.addEventListener("click",find_editt)
+node_for_click.addEventListener("click",find_editfo)
